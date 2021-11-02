78ea76da9dbad2f78f12fa8441c899c066b09e82
caro.deman20 <caro.deman20@triana.salesianos.edu>
2021-11-02 10:36:45 +0100

    inicioProyecto

---
+++ E06ModeloDatos/src/main/java/com/example/E06ModeloDatos/E06ModeloDatosApplication.java
@@ -0,0 +1,13 @@
+package com.example.E06ModeloDatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E06ModeloDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E06ModeloDatosApplication.class, args);
+	}
+
+}
---
+++ E06ModeloDatos/src/test/java/com/example/E06ModeloDatos/E06ModeloDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E06ModeloDatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E06ModeloDatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
